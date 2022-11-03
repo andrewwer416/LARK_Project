2fab45c4e5373f5aff32def62f809fc5b66bcd23
andrewwer416 <awernersbach0361@floridapoly.edu>
2022-11-03 19:16:40 -0400

    new folders add lore

--- intro.sh
+++ intro.sh
@@ -7,12 +7,18 @@ read -p "Please make a choice" opt
 case $opt in
   Green|green)
     echo "You earth"
+    mv .Green Green
+    echo "Use cd Green to go into the Realm of earth"
   ;;
   Blue|blue)
     echo "You heaven"
+    mv .Blue Blue
+    echo "Use cd Blue to go into the Realm of heaven"
   ;;
   Red|red)
     echo "You hell"
+    mv .Red Red
+    echo "Use cd Red to go into the Realm of hell"
   ;;
 esac
 exit
