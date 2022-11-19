20c094118277a2556d78709d8f8b2d24ae4d0c4c
andrewwer416 <awernersbach0361@floridapoly.edu>
2022-11-19 00:40:28 -0500

    changed cleanup and intro

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,46 @@
 #!/usr/bin/bash
-cp Characters/.Names Characters/Names.txt
-echo -n "" > Data/my_stats.txt
-echo -n "" > Data/enemy_stats.txt
-mv Orb .Orb
-mv .Orb/Tutorial .Orb/.Tutorial
-mv .Orb/Battle1 .Orb/.Battle1
+cp .Characters/.Names .Characters/Names.txt
+echo -n "" > .Data/my_stats.txt
+echo -n "" > .Data/enemy_stats.txt
+if [ -e Orb ]; then
+	mv Orb .Orb
+fi
+if [ -e .Orb/Tutorial ]; then
+	mv .Orb/Tutorial .Orb/.Tutorial
+fi
+if [ -e .Orb/Field ]; then
+	mv .Orb/Field .Orb/.Field
+fi
+if [ -e .Inventory/Locket ]; then
+	mv .Inventory/Locket .Orb/.Field/Locket
+fi
+if [ -e .Orb/.Field/Sky ]; then
+	mv .Orb/.Field/Sky .Orb/.Field/.Sky
+fi
+unset team_name
+unset character_name
+unset usr_m1
+unset usr_m2
+unset usr_m3
+unset enem_m1
+unset enem_m2
+unset enem_m3
+unset team_block
+unset enem_block
+unset charopt
+unset enem1_name
+unset enem2_name
+unset battle1
+unset battle2
+unset opt
+unset usr_atk
+unset usr_dmg
+unset usr_acc
+unset usr_move
+unset enem_atk
+unset enem_dmg
+unset enem_acc
+unset enem_move
+unset tut_done
+unset inp
 return
--- intro.sh
+++ intro.sh
@@ -2,23 +2,27 @@
 # EDIT WORDS!!!!!!
 # Intro Phase: select an orb
 export head="$PWD"
-export my_stats="$PWD"/Data/my_stats.txt
-export enemy_stats="$PWD"/Data/enemy_stats.txt
-export moves="$PWD"/Characters/Moves.txt
-export characters="$PWD"/Characters/Character.txt
-echo "You awake. 3 Orbs green red blue."
+export inventory="$PWD"/.Inventory
+export my_stats="$PWD"/.Data/my_stats.txt
+export enemy_stats="$PWD"/.Data/enemy_stats.txt
+export moves="$PWD"/.Characters/Moves.txt
+export characters="$PWD"/.Characters/Character.txt
+echo "You awaken within a void. You can see nothing but three orbs rotating around you. As you reach your non-existent hand out to the orbs you feel struggle and anguish within each. The red orb feels warm but foreboding. \
+The green orb is full of hope and promise. The blue orb seems to hold plentiful wealth. Fate calls you to choose one of these orbs."
+tut_done=0
+if [ -e .Orb ]; then
+	mv .Orb Orb
+fi
 while true
 do
-	read -p "Please make a choice: " orb
+	read -p "Which orb holds your destiny? " orb
 	case $orb in
 		Green|green|Blue|blue|Red|red)
-			echo "You earth"
-			mv .Orb Orb
-			echo "Use cd Orb to grab the orb"
+			echo "To finalize your selection of the $orb orb, type 'cd Orb'. Retype 'source intro.sh' to change your decision."
 			return
 			;;
 		*)
-			echo "Please enter valid orb"
+			echo "Please select a valid orb"
 	esac
 done
 return
